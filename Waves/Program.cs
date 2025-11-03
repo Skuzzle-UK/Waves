e4e85aed318ebbd7fc5978162fbdb1c1ad9379a8
Nick Bailey <nick@skuzzle.com>
2025-11-03 17:06:57 +0000

    Initialise repo with razor console project

---
+++ Waves/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using RazorConsole.Core;
+
+namespace Waves;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args)
+            .UseRazorConsole<Sample>();
+
+        IHost host = hostBuilder.Build();
+        await host.RunAsync();
+    }
+}
